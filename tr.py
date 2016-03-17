458d04f89b5f3635c73f1a191d9c9c9187614116
Yi Wei <genius.ron@gmail.com>
2016-03-17 15:10:46 +0800

    rm num id columns and adjust size

--- tr.py
+++ tr.py
@@ -19,9 +19,11 @@ def dl(x):
     cv2.line(im, (x, 0), (x, height), white)
     cv2.line(im, (x+1, 0), (x+1, height), white)
 
+
 # add white split up lines between different close columns
 c = 7
 x = 89
+x0 = 10
 while c > 0:
     if c == 2:
         x -= 2
@@ -31,6 +33,12 @@ while c > 0:
     dl(x+23)
     x += 172
     c -= 1
+    ww = 52
+    if c == 0:
+        ww = 55
+    for i in range(x0, x0+ww):
+        dl(i)
+    x0 +=172
 
 im3 = im.copy()
 
@@ -52,7 +60,7 @@ keys = [i for i in range(48,58) + range(97,123)] #ascii code 0-9, a=z
 
 for c in contours:
     [x,y,w,h] = cv2.boundingRect(c)
-    if  h>20 and w >10 and w < 40:
+    if  h>28 and w >15 and w < 40:
         cv2.rectangle(im,(x,y),(x+w,y+h),(152,251,152),2)
 
 
@@ -60,7 +68,7 @@ for cnt in contours:
     if cv2.contourArea(cnt)>10:
         [x,y,w,h] = cv2.boundingRect(cnt)
 
-        if  h>25 and w >10 and w < 35:
+        if  h>28 and w >15 and w < 35:
             cv2.rectangle(im,(x,y),(x+w,y+h),(0,0,255),2)
             roi = thresh[y:y+h,x:x+w]
             roismall = cv2.resize(roi,(10,10))
